8cd5c4e9b7cc208d9304b1164d8913b2ce11cb72
miguelangelmagdalena <miguelangelmagdalena@gmail.com>
2018-07-15 22:15:48 -0400

    Agregando handlebars pero sin utilizar y collections de postman

--- app.js
+++ app.js
@@ -2,11 +2,23 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const hbs = require('express-handlebars')
 const app = express()
 const api = require('./routes')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+/*Configuracion para plantillas html*/
+app.engine('hbs', hbs({
+	defaultLayout: 'default',
+	extname: 'hbs',
+	layoutsDir: __dirname + '/views/layouts/'
+}))
+app.set('view engine', 'hbs')
+
+
 app.use('/institution', api)
+
 
 module.exports = app
